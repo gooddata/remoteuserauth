5321eb3f7a2ee00adae7ac7c359ec6bbb6d21a59
Martin Ducar <martin.ducar@gooddata.com>
2017-02-20 13:21:37 +0100

    Make Realm removal optional

--- RemoteUserJiraAuth/src/main/java/anguswarren/jira/RemoteUserJiraAuth.java
+++ RemoteUserJiraAuth/src/main/java/anguswarren/jira/RemoteUserJiraAuth.java
@@ -68,8 +68,15 @@ public class RemoteUserJiraAuth extends JiraSeraphAuthenticator {
                 }
 
                 if (remoteuser != null) {
-                    String[] username = remoteuser.split("@");
-                    user = getUser(username[0]);
+                    Boolean removeRealm = new Boolean(true);
+                    if (p.getProperty("removeRealm") != null) {
+                        removeRealm = Boolean.parseBoolean(p.getProperty("removeRealm"));
+                    }
+                    if (removeRealm) {
+                        user = getUser(remoteuser.split("@")[0]);
+                    } else {
+                        user = getUser(remoteuser);
+                    }
                     log.debug("Logging in with username: " + user);
                     request.getSession().setAttribute(JiraSeraphAuthenticator.LOGGED_IN_KEY, user);
                     request.getSession().setAttribute(JiraSeraphAuthenticator.LOGGED_OUT_KEY, null);
